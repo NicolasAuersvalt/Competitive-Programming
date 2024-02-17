1efcca0377c0baaafac0d223d803a6c3669befbe
Replit user <>
2024-02-17 21:30:56 +0000

    a

--- 118A/118A.c
+++ 118A/118A.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 #define TAM 100
 
 int main() {
-    char str[TAM];
-    char novaStr[2 * TAM];
-    int i, len, novaLen;
+  char str[TAM];
+  char novaStr[2 * TAM];
+  int i, len, novaLen;
 
-    scanf("%s", str);
+  scanf("%s", str);
 
-    for (len = 0; str[len] != '\0'; len++) {
+  for (len = 0; str[len] != '\0'; len++) {
+  }
+
+  novaLen = 0;
+
+  for (i = 0; i < len; i++) {
+    str[i] = tolower(str[i]);
+
+    if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' &&
+        str[i] != 'u' && str[i] != 'y') {
+      novaStr[novaLen++] = '.';
+      novaStr[novaLen++] = str[i];
     }
+  }
 
-    novaLen = 0;
+  novaStr[novaLen] = '\0';
 
-    for (i = 0; i < len; i++) {
-        str[i] = tolower(str[i]);
+  printf("%s\n", novaStr);
 
-        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
-            str[i] != 'o' && str[i] != 'u' && str[i] != 'y') {
-            novaStr[novaLen++] = '.';
-            novaStr[novaLen++] = str[i];
-        }
-    }
-
-    novaStr[novaLen] = '\0';
-
-    printf("%s\n", novaStr);
-
-    return 0;
+  return 0;
 }
