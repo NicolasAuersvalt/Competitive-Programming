ded3a4f34e50d966eb8732af8543e670f0945742
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-02-25 08:03:47 +0000

    a

---
+++ CodeForces/228A/228A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+  int vet[4], i, j, count=0;
+  for(i=0; i<4; i++){
+    cin >> vet[i];
+  }
+
+  for(i=0; i<4; i++){
+    
+  }
+
+  cout << count;
+  return 0;
+}
