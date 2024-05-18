d7f58f03faf9cc55c61a0d56c18421096cfa72ca
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-05-18 04:34:37 +0000

    sbnfaijsfa

---
+++ Beecrowd/2/2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int n, m;
+  cin >> n >> m;
+  cout << "X = " << n+m;
+
+  return 0;
+}
---
+++ OBI/2021/baralho.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+char baralho[155];
+int i;
+
+for(i=0; baralho[i]!='\0';i++){
+  scanf("%s",baralho);
+}
+  return 0;
+}
