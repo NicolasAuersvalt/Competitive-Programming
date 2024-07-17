5109e1607f95d957968c3e205dd119ba6e2fa978
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-07-17 05:00:47 +0000

    asdas

--- Exemplos/C++/set.cpp
+++ Exemplos/C++/set.cpp
@@ -1,15 +1,22 @@
 #include<bits/stdc++.h>
 
+// NÃO DÁ PARA FAZER SET[1], pois não é um vetor
+
 // ====/====/====/====/====/====/====/====/====/====/====/
 
-// # .find()
+// # .find() // Retorna um ponteiro para o elemento ou o fim do vetor se não encontrar
+  if(set1.find(*l) != set1.end()){
+
 // # .begin(), end()
+
 // # .count()
+
 // # .insert
-
-
+mySet.insert(input); // Insere o valor no set
+    
 // ====/====/====/====/====/====/====/====/====/====/====/
 
 // # Declaração 
 
 set<int> set_int;
+
