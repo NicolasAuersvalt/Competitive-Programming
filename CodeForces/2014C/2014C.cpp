810cc8270067ecf01637d9b47e03e5d85d0ff8ea
NicolasAuersvalt <nicolasauersvalt@gmail.com>
2025-11-19 21:40:41 -0300

    sla

--- CodeForces/2014C/2014C.cpp
+++ CodeForces/2014C/2014C.cpp
@@ -11,23 +11,26 @@ int main(){
 
     int t; cin >> t;
     while(t--){
-        //cout << "A" << endl;
-        int n, sum=0; cin >> n;
+        int n; cin >> n;
+        long long sum=0;
         vi vet;
-        while(n--){
-            //cout << "B" << endl;
+        for(int i=0; i<n;i++){
             int a; cin >> a;
             sum+=a;
             vet.push_back(a);
         }
-        //cout << "C" << endl;
-        sort(vet.begin(), vet.end());
-        int media = vet[vet.size()/2];
-        cout << "Media = " << media << endl;
-        cout << "Ultimo Valor = " << vet[vet.size()-1]<< endl;
-        cout << "Soma = " << sum << endl;
-        int x = 2*media * vet.size() - sum + 1;
-        cout << x << endl;
+        if(n<=2){
+            cout << -1 << endl;
+        }
+        else{
+            sort(vet.begin(), vet.end());
+            int media = vet[vet.size()/2];
+            long long x = 2*media * vet.size() - sum + 1;
+            if(x>=0)
+                cout << x << endl;
+            else
+                cout << 0 << endl;
+        }
     }
     
     return 0;
